6fedfa751507f4b89251c0346b92ff587e51156d
jyk0128 <wlsyong90@gmail.com>
2025-07-09 19:44:50 +0900

    dev. 채팅방 초안작성

--- apps/web/src/routes/_protected/_page/channel.$id.tsx
+++ apps/web/src/routes/_protected/_page/channel.$id.tsx
@@ -1,6 +1,13 @@
 import { withMenu } from '#/routes/_protected/-layout/with-menu';
-import { useQuery, useTRPC } from '@packages/trpc';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation, useQuery, useTRPC } from '@packages/trpc';
+import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, FormController, FormRicharea } from '@packages/ui';
 import { createFileRoute, notFound } from '@tanstack/react-router';
+import { uniqueId } from 'lodash-es';
+import { Send } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 export const Route = createFileRoute('/_protected/_page/channel/$id')({
   beforeLoad: async ({ context: { trpc, queryClient }, params }) => {
@@ -13,16 +20,93 @@ export const Route = createFileRoute('/_protected/_page/channel/$id')({
   component: withMenu(RouteComponent),
 });
 
+const fields = z.object({
+  content: z.string().min(1),
+}).default({
+  content: '',
+});
+type FieldValues = z.infer<typeof fields>;
+
 function RouteComponent() {
   const trpc = useTRPC();
   const params = Route.useParams();
   const { data: channel } = useQuery(trpc.getChannel.queryOptions({ channelId: params.id }));
 
+  const form = useForm({
+    resolver: zodResolver(fields.removeDefault()),
+    defaultValues: fields._def.defaultValue(),
+  });
+
+  const [messages, setMessages] = useState<string[]>([]);
+  const { mutateAsync: sendMessage } = useMutation(trpc.sendMessage.mutationOptions());
+  useEffect(() => {
+    const subscription = trpc.receiveMessage.subscriptionOptions().subscribe({
+      onData: (data) => {
+        setMessages((prev) => [...prev, data.content]);
+      },
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSubmit: SubmitHandler<FieldValues> = (payload, evt) => {
+    const { submitter } = (evt?.nativeEvent ?? {}) as SubmitEvent;
+    if (!(submitter instanceof HTMLButtonElement)) return;
+    switch (submitter.name) {
+      case 'submit':
+        sendMessage(payload);
+        form.reset();
+        return;
+      default:
+        throw new Error('unexpected error');
+    }
+  };
+
   return (
-    <div>
-      <div>{channel?.name}</div>
-      <div>{channel?.description}</div>
-      <div>채팅하자</div>
-    </div>
+    <Card className="tw:size-full tw:grid tw:grid-rows-[auto_1fr_auto]">
+      <CardHeader>
+        <CardTitle>{channel?.name}</CardTitle>
+        <CardDescription>{channel?.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {messages.map((msg) => (
+          <div key={uniqueId()}>{msg}</div>),
+        )}
+      </CardContent>
+      <CardFooter>
+        <FormController
+          form={form}
+          onSubmit={handleSubmit}
+          className="tw:relative tw:flex tw:justify-end tw:items-center tw:size-full"
+        >
+          <FormRicharea
+            name="content"
+            control={form.control}
+            orientation="horizontal"
+            className="tw:pr-20"
+            size="full"
+            onKeyDown={(e) => {
+              if (!(e.altKey || e.shiftKey) && e.key === 'Enter') {
+                e.preventDefault();
+                const form = e.currentTarget.closest('form');
+                const submitter = form?.querySelector('button[name="submit"]');
+                if (submitter instanceof HTMLButtonElement) {
+                  e.currentTarget.closest('form')?.requestSubmit(submitter);
+                }
+              }
+            }}
+          />
+          <Button
+            type="submit"
+            name="submit"
+            className="tw:absolute tw:right-5"
+          >
+            <Send />
+          </Button>
+        </FormController>
+      </CardFooter>
+    </Card>
   );
 }
