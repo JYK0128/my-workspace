40af22608385a362306e7e8ff306c9c816697542
jyk0128 <wlsyong90@gmail.com>
2025-07-11 17:57:20 +0900

    mod. 질문 유형 추가(개선 필요)

--- packages/trpc/src/core/emitter.ts
+++ packages/trpc/src/core/emitter.ts
@@ -5,6 +5,7 @@ export { on };
 /* 이벤트 정의 */
 export type EmitterMap = {
   message: [{ channelId: string, userId: string, nickname: string, content: string }]
+  question: [{ channelId: string, userId: string, nickname: string, content: string }]
 };
 export const emitter = new EventEmitter<EmitterMap>();
 
