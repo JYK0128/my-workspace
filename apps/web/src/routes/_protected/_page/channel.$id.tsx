f8437608d6bc5d82525666759e60924899a5310f
jyk0128 <wlsyong90@gmail.com>
2025-07-12 10:10:21 +0900

    update. 채널 상세기능

--- apps/web/src/routes/_protected/_page/channel.$id.tsx
+++ apps/web/src/routes/_protected/_page/channel.$id.tsx
@@ -1,10 +1,12 @@
 import { withMenu } from '#/routes/_protected/-layout/with-menu';
+import { ChannelParticipant } from '#/routes/_protected/-modal/channel-participant';
+import { ChannelSetting } from '#/routes/_protected/-modal/channel-setting';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQuery, useTRPC } from '@packages/trpc';
-import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, cn, FormController, FormRicharea, Skeleton } from '@packages/ui';
-import { createFileRoute, notFound } from '@tanstack/react-router';
+import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, cn, FormController, FormRicharea, Skeleton, StepModal } from '@packages/ui';
+import { createFileRoute, notFound, useRouter } from '@tanstack/react-router';
 import { uniqueId } from 'lodash-es';
-import { CornerDownRight, Heart, Send } from 'lucide-react';
+import { CornerDownRight, Heart, MoveLeft, Send, Settings, UserSearch } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useForm, type SubmitHandler } from 'react-hook-form';
 import { useAuth } from 'react-oidc-context';
@@ -12,12 +14,15 @@ import { z } from 'zod';
 
 export const Route = createFileRoute('/_protected/_page/channel/$id')({
   beforeLoad: async ({ context: { trpc, queryClient }, params }) => {
-    const data = await queryClient.fetchQuery(trpc.isParticipant.queryOptions({ channelId: params.id }));
-
-    if (!data) {
+    const { id: channelId } = params;
+    const participantInfo = await queryClient.fetchQuery(trpc.getParticipantInfo.queryOptions({ channelId }));
+    if (!participantInfo) {
       throw notFound();
     }
+
+    return { participantInfo };
   },
+  loader: ({ context: { participantInfo } }) => ({ participantInfo }),
   component: withMenu(RouteComponent),
 });
 
@@ -33,28 +38,25 @@ type FieldValues = z.infer<typeof fields>;
 function RouteComponent() {
   const trpc = useTRPC();
   const { user } = useAuth();
-  const params = Route.useParams();
-  const { data: channel } = useQuery(trpc.getChannel.queryOptions({ channelId: params.id }));
+  const { id: channelId } = Route.useParams();
+  const { data: channel, refetch } = useQuery(trpc.getChannel.queryOptions({ channelId }));
+  const router = useRouter();
+  const { participantInfo } = Route.useLoaderData();
 
   const form = useForm({
     resolver: zodResolver(fields.removeDefault()),
-    defaultValues: fields._def.defaultValue(),
+    defaultValues: {
+      ...fields._def.defaultValue(),
+      channelId,
+    },
   });
-
-  useEffect(() => {
-    if (channel?.id) {
-      form.reset(
-        { ...fields._def.defaultValue(), channelId: channel.id },
-      );
-    }
-  }, [channel, form]);
 
   const [messages, setMessages] = useState<(FieldValues & { seq?: number, userId: string, nickname: string })[]>([]);
   const { mutateAsync: sendMessage } = useMutation(trpc.sendMessage.mutationOptions());
 
   useEffect(() => {
-    if (!channel) return;
-    const subscription = trpc.receiveMessage.subscriptionOptions({ channelId: channel.id }).subscribe({
+    if (!channelId) return;
+    const subscription = trpc.receiveMessage.subscriptionOptions({ channelId }).subscribe({
       onData: (data) => {
         setMessages((prev) => [...prev, data]);
       },
@@ -62,38 +64,35 @@ function RouteComponent() {
     return () => {
       subscription.unsubscribe();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [channel]);
+  }, [channelId, trpc.receiveMessage]);
 
   useEffect(() => {
-    if (!channel) return;
-    const subscription = trpc.receiveAnswer.subscriptionOptions({ channelId: channel.id }).subscribe({
+    if (!channelId) return;
+    const subscription = trpc.receiveAnswer.subscriptionOptions({ channelId }).subscribe({
       onData: (data) => {
-        setMessages((old) => {
-          const idx = old.findIndex((v) => v.userId === data.userId);
-          if (idx === -1 && data.content) {
-            console.log('cond 01: ', idx, data.content);
-            old.push({
+        setMessages((messages) => {
+          const idx = messages.findIndex((v) => v.userId === data.userId);
+          if (idx < 0 && data.content) {
+            messages.push({
               ...data,
-              content: data.content ?? '',
+              content: data.content,
             });
-            return [...old];
+            return [...messages];
           }
-          else if (idx !== -1 && data.content) {
-            if (old[idx].seq !== data.seq) {
-              old[idx] = {
-                ...old[idx],
-                content: [old[idx].content, data.content].join(''),
+          else if (idx >= 0 && data.content) {
+            if (messages[idx].seq !== data.seq) {
+              messages[idx] = {
+                ...data,
+                content: [messages[idx].content, data.content].join(''),
               };
-              return [...old];
+              return [...messages];
             }
             else {
-              return old;
+              return messages;
             }
           }
           else {
-            console.log('cond 03: ', idx, data.content);
-            return old;
+            return messages;
           }
         });
       },
@@ -101,7 +100,7 @@ function RouteComponent() {
     return () => {
       subscription.unsubscribe();
     };
-  }, [channel, trpc.receiveAnswer]);
+  }, [channelId, trpc.receiveAnswer]);
 
   const handleSubmit: SubmitHandler<FieldValues> = (payload, evt) => {
     const { submitter } = (evt?.nativeEvent ?? {}) as SubmitEvent;
@@ -119,58 +118,104 @@ function RouteComponent() {
   };
 
   return (
-    <Card className="tw:size-full tw:grid tw:grid-rows-[auto_1fr_auto]">
-      <CardHeader>
-        <CardTitle>{channel?.name}</CardTitle>
-        <CardDescription>{channel?.description}</CardDescription>
-      </CardHeader>
-      <CardContent className="tw:overflow-y-auto">
-        {messages.map((msg) => (
-          <Message
-            key={uniqueId()}
-            {...{
-              type: msg.userId === user?.profile.sub ? 'sent' : 'received',
-              nickname: msg.nickname,
-              message: msg.content,
-              createdAt: new Date(),
-            }}
-          />
-        ))}
-      </CardContent>
-      <CardFooter>
-        <FormController
-          form={form}
-          onSubmit={handleSubmit}
-          onError={(err) => console.error(err)}
-          className="tw:relative tw:flex tw:justify-end tw:items-center tw:size-full"
+    <div className="tw:size-full tw:grid tw:grid-rows-[auto_1fr]">
+      <div className="tw:flex tw:justify-between">
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={() => router.history.back()}
         >
-          <FormRicharea
-            name="content"
-            control={form.control}
-            orientation="horizontal"
-            className="tw:pr-20 tw:h-25"
-            size="full"
-            onKeyDown={(e) => {
-              if (!(e.altKey || e.shiftKey) && e.key === 'Enter') {
-                e.preventDefault();
-                const form = e.currentTarget.closest('form');
-                const submitter = form?.querySelector('button[name="submit"]');
-                if (submitter instanceof HTMLButtonElement) {
-                  e.currentTarget.closest('form')?.requestSubmit(submitter);
-                }
-              }
-            }}
-          />
-          <Button
-            type="submit"
-            name="submit"
-            className="tw:absolute tw:right-5"
+          <MoveLeft />
+        </Button>
+        <div>
+          {/* 유저 */}
+          {channel && (
+            <StepModal
+              callback={refetch}
+              closable={true}
+              render={[
+                <ChannelParticipant key="setting" {...{ channel }} />,
+              ]}
+            >
+              <Button
+                variant="ghost"
+                size="icon"
+              >
+                <UserSearch />
+              </Button>
+            </StepModal>
+          )}
+          {/* 설정 */}
+          {channel && (
+            <StepModal
+              callback={refetch}
+              render={[
+                <ChannelSetting key="setting" {...{ channel, participantInfo }} />,
+              ]}
+            >
+              <Button
+                variant="ghost"
+                size="icon"
+              >
+                <Settings />
+              </Button>
+            </StepModal>
+          )}
+        </div>
+      </div>
+      <Card className="tw:size-full tw:grid tw:grid-rows-[auto_1fr_auto]">
+        <CardHeader>
+          <CardTitle>{channel?.name}</CardTitle>
+          <CardDescription>{channel?.description}</CardDescription>
+        </CardHeader>
+        <CardContent className="tw:overflow-y-auto">
+          {messages.map((msg) => (
+            <Message
+              key={uniqueId()}
+              {...{
+                type: msg.userId === user?.profile.sub ? 'sent' : 'received',
+                nickname: msg.nickname,
+                message: msg.content,
+                createdAt: new Date(),
+              }}
+            />
+          ))}
+        </CardContent>
+        <CardFooter>
+          <FormController
+            form={form}
+            onSubmit={handleSubmit}
+            onError={(err) => console.error(err)}
+            className="tw:relative tw:flex tw:justify-end tw:items-center tw:size-full"
           >
-            <Send />
-          </Button>
-        </FormController>
-      </CardFooter>
-    </Card>
+            <FormRicharea
+              name="content"
+              control={form.control}
+              orientation="horizontal"
+              className="tw:pr-20 tw:h-25"
+              size="full"
+              onKeyDown={(e) => {
+                if (!(e.altKey || e.shiftKey) && e.key === 'Enter') {
+                  e.preventDefault();
+                  const form = e.currentTarget.closest('form');
+                  const submitter = form?.querySelector('button[name="submit"]');
+                  if (submitter instanceof HTMLButtonElement) {
+                    e.currentTarget.closest('form')?.requestSubmit(submitter);
+                  }
+                }
+              }}
+            />
+            <Button
+              type="submit"
+              name="submit"
+              className="tw:absolute tw:right-5"
+            >
+              <Send />
+            </Button>
+          </FormController>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
 
