5a4a6d5d7a26551607039ef1eefbdd6c1711b511
jyk0128 <wlsyong90@gmail.com>
2025-07-22 15:17:49 +0900

    개선작업(2)

---
+++ packages/ui/lib/stories/FormController.stories.tsx
@@ -0,0 +1,81 @@
+import { FormController } from '#customs/components/FormController.tsx';
+import { FormInput } from '#customs/components/FormInput.tsx';
+import { Button } from '#shadcn/components/ui/button.tsx';
+import { zodResolver } from '@hookform/resolvers/zod';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { useForm, type SubmitErrorHandler, type SubmitHandler } from 'react-hook-form';
+import { action } from 'storybook/actions';
+import { z } from 'zod';
+
+// 1. Meta
+/**
+ * react-hook-form으로 form을 관리합니다.
+ * FormController 전용 Input 컴포넌트를 이용합니다.
+ */
+const meta = {
+  title: 'Design/Form/Controller',
+  component: FormController,
+  argTypes: {
+    children: {
+      description: 'Form Controller 전용 입력 컴포넌트',
+      control: { disable: true },
+    },
+    form: {
+      description: 'react-hook-form의 form 객체',
+      control: { disable: true },
+    },
+    onSubmit: {
+      description: 'Submit 핸들러',
+    },
+    onError: {
+      description: 'Error 핸들러',
+    },
+  },
+  tags: ['!dev'],
+} satisfies Meta<typeof FormController>;
+
+// 2. Settings
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// 3. Story
+export const Input: Story = {
+  args: {} as never,
+  render: () => {
+    const schema = z.object({
+      input: z.string().min(1, '최소 1자 이상 입력해주세요.'),
+    }).default({
+      input: '',
+    });
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const form = useForm({
+      resolver: zodResolver(schema.removeDefault()),
+      defaultValues: schema._def.defaultValue(),
+    });
+
+    const onSubmit: SubmitHandler<z.infer<typeof schema>>
+    = (...args) => action('submit')(args);
+    const onError: SubmitErrorHandler<z.infer<typeof schema>>
+    = (...args) => action('error')(args);
+
+    return (
+      <FormController
+        form={form}
+        onSubmit={onSubmit}
+        onError={onError}
+      >
+        <FormInput
+          control={form.control}
+          name="input"
+          label="라벨"
+          labelWidth="180px"
+          showError={true}
+          orientation="vertical"
+        />
+        <Button type="reset">초기화</Button>
+        <Button type="submit">제출</Button>
+      </FormController>
+    );
+  },
+};
---
+++ packages/ui/lib/stories/FormTextarea.stories.tsx
@@ -0,0 +1,125 @@
+import { FormController } from '#customs/components/FormController.tsx';
+import { FormTextarea } from '#customs/components/FormTextarea.tsx';
+import { Button } from '#shadcn/components/ui/button.tsx';
+import { zodResolver } from '@hookform/resolvers/zod';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { useForm, useFormContext, type SubmitErrorHandler, type SubmitHandler } from 'react-hook-form';
+import { action } from 'storybook/actions';
+import { z } from 'zod';
+
+// 1. Meta
+/**
+ * FormController 전용 Textarea 컴포넌트입니다.
+ * 기본적인 Textarea 속성과 함께 다음 속성을 지원합니다.
+ */
+const meta = {
+  tags: ['!dev'],
+  title: 'Design/Form/Textarea',
+  component: FormTextarea,
+  argTypes: {
+    control: {
+      description: 'react-hook-form의 control 객체',
+      control: { disable: true },
+    },
+    name: {
+      description: '입력 필드의 이름',
+      control: { disable: true },
+    },
+    label: {
+      description: '필드의 라벨',
+      control: { type: 'text' },
+    },
+    labelWidth: {
+      description: '라벨의 너비',
+      control: { type: 'text' },
+    },
+    showError: {
+      description: '폼 에러 메시지를 표시할지 여부',
+      control: { type: 'boolean' },
+    },
+    orientation: {
+      description: '필드와 라벨의 배치 방향',
+      control: { type: 'radio' },
+      options: ['horizontal', 'vertical'],
+    },
+  },
+  decorators: [(Story) => {
+    const schema = z.object({
+      input: z.string().min(1, '최소 1자 이상 입력해주세요.'),
+    }).default({
+      input: '',
+    });
+
+    const form = useForm({
+      resolver: zodResolver(schema.removeDefault()),
+      defaultValues: schema._def.defaultValue(),
+    });
+
+    const onSubmit: SubmitHandler<z.infer<typeof schema>>
+    = (...args) => action('submit')(args);
+    const onError: SubmitErrorHandler<z.infer<typeof schema>>
+    = (...args) => action('error')(args);
+
+    return (
+      <FormController
+        form={form}
+        onSubmit={onSubmit}
+        onError={onError}
+      >
+        <Story />
+        <Button type="reset">초기화</Button>
+        <Button type="submit">제출</Button>
+      </FormController>
+    );
+  }],
+  parameters: {
+    docs: {
+      source: {
+        code:
+        `<FormTextarea
+          control={form.control}
+          name={name}
+          label={label}
+          labelWidth={labelWidth}
+          showError={showError}
+          orientation={orientation}
+          placeholder={placeholder}
+        />`,
+      },
+    },
+  },
+} satisfies Meta<typeof FormTextarea>;
+
+// 2. Settings
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// 3. Story
+export const Default: Story = {
+  args: {
+    control: {} as never,
+    name: 'input',
+    label: '라벨',
+    labelWidth: '180px',
+    showError: true,
+    orientation: 'horizontal',
+    placeholder: '입력해주세요.',
+  },
+  render: (args) => {
+    const { label, name, labelWidth, showError, orientation, placeholder } = args;
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const form = useFormContext();
+
+    return (
+      <FormTextarea
+        control={form.control}
+        name={name}
+        label={label}
+        labelWidth={labelWidth}
+        showError={showError}
+        orientation={orientation}
+        placeholder={placeholder}
+      />
+    );
+  },
+};
