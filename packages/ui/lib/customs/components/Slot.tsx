a90aa29a59e7c2b6da980f8b04b98232753982a8
jyk0128 <wlsyong90@gmail.com>
2025-07-25 19:51:20 +0900

    feat slot 컴포넌트 태그 지정 기능

--- packages/ui/lib/customs/components/Slot.tsx
+++ packages/ui/lib/customs/components/Slot.tsx
@@ -1,14 +1,18 @@
-import React, { HTMLAttributes, PropsWithChildren, ReactElement } from 'react';
+import React, { HTMLAttributes, ReactElement, type ComponentPropsWithRef, type ElementType } from 'react';
 
-export interface SlotProps extends PropsWithChildren<HTMLAttributes<HTMLElement>> {
+export type SlotProps<T extends ElementType = 'div'> = {
   asChild?: boolean
-}
+  as?: T
+} & ComponentPropsWithRef<T>;
 
-export function Slot({ children, asChild, ...props }: SlotProps) {
+export function Slot({ children, as, asChild, ...props }: SlotProps) {
   if (asChild) {
     const childrenArray = React.Children.toArray(children);
     const childCount = childrenArray.length;
-    if (childCount === 1) {
+    if (childCount !== 1) {
+      throw Error('expected to receive a single React element child.');
+    }
+    else {
       const child = childrenArray[0] as ReactElement<HTMLAttributes<HTMLElement>>;
       return React.cloneElement(child, {
         ...props,
@@ -16,13 +20,14 @@ export function Slot({ children, asChild, ...props }: SlotProps) {
         style: { ...props.style, ...child.props.style },
       });
     }
-    else {
-      throw Error('expected to receive a single React element child.');
-    }
   }
   else {
-    <div {...props}>
-      {children}
-    </div>;
+    const Component = as || 'div';
+
+    return (
+      <Component {...props}>
+        {children}
+      </Component>
+    );
   }
 }
